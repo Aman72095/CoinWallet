17fb865469ea5b2f33bd393d426bc4b8968b8af7
Aman72095 <amanpatel52268@gmail.com>
2024-07-21 11:22:50 +0530

    Update index.js

--- Pages/index.js
+++ Pages/index.js
@@ -102,7 +102,7 @@ export default function Homepage() {
                 getCoins();
             }
         };
-        iniCoins();
+        initCoins();
     }, [CoinWalletContract]);
 
     return (
